e8dbfb7c2778599144e7dea2a9df0ad40c9ea9c5
kenseehart <ken@seehart.com>
2022-08-13 15:30:14 -0700

    Update setup.py

--- midi2poprap/setup.py
+++ midi2poprap/setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     name = 'midi2poprap',
     packages = ['midi2poprap'],
-    version = '1.0',
+    version = '1.0.0',
     description = 'Convert midi to poprap pdf, the industry standard for bubble wrap music devices',
     author = 'Ken Seehart',
     author_email = 'ken@seehart.com',
-    url = 'https://github.com/kenseehart/midi2poprap',
+    url = 'https://github.com/kenseehart/midi2poprap/poprap',
     download_url = 'https://github.com/kenseehart/midi2poprap/archive/refs/tags/v1.0.0.tar.gz',
     keywords = ['tag1', 'tag2'],
     classifiers = [],
